4e0be4f8ac2813e3ae56dbef305185c24de722e0
leeg827 <70984713+leeg827@users.noreply.github.com>
2020-11-10 13:43:44 -0800

    toy prototype

---
+++ toytest/js/global.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function(){
+
+    function changeBackground (){
+        document.body.style.backgroundColor = "lightblue"
+    }
+    
+    document.getElementById("backgroundbutton").addEventListener("onClick",changeBackground);
+})
+
---
+++ toywebsite/js/global.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function() {
+
+   function cloudSky(){
+      document.body.style.backgroundImage = "url('images/cloudplaceholder.png')"
+      document.body.style.backgroundImage.width="100%"
+   }
+
+   document.getElementById("cloud").addEventListener("click",cloudSky)
+
+   function starSky(){
+      document.body.style.backgroundImage = "url('images/starplaceholder.png')"
+   }
+
+   document.getElementById("star2").addEventListener("click",starSky)
+
+   function rainSky(){
+      document.body.style.backgroundImage = "url('images/rainplaceholder.png')"
+   }
+
+   document.getElementById("drop2").addEventListener("click",rainSky)
+});
